f876d7e694779f251807f07d9eba7e7f9299150f
kawanakeiware <kawanakeiware@localhost>
2025-06-18 07:40:08 +0900

    最終課題その１

--- practice/src/main/java/com/example/demo/repository/ContactRepository.java
+++ practice/src/main/java/com/example/demo/repository/ContactRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Contact;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {
- 
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }
